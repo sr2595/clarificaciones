4e8b6ab589887790043b4d642e14611938504c48
sr2595 <sr.torrijos25@gmail.com>
2025-08-26 09:11:58 +0200

    incluir selector grupal bueno

--- CLARIFICACIONES/CODE/streamlit_app_UTE_con_pagos.py
+++ CLARIFICACIONES/CODE/streamlit_app_UTE_con_pagos.py
@@ -174,7 +174,7 @@ if archivo:
     # --- Filtrar UTES del mismo grupo y eliminar negativas ---
     df_utes_grupo = df[
         (df[col_grupo] == grupo_seleccionado) & (df['ES_UTE'])
-    ].copy(
+    ].copy()
 
     
     df_utes_grupo = df_utes_grupo[df_utes_grupo['IMPORTE_CORRECTO'].fillna(0) > 0]
